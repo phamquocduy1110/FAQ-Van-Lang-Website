1f880e8025b072c10fb12091149fcf605a29c18f
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-06-28 15:00:35 +0700

    Delete Database

--- CNTTFAQ/CNTTFAQ/Areas/Admin/Controllers/ManageQuestionsController.cs
+++ CNTTFAQ/CNTTFAQ/Areas/Admin/Controllers/ManageQuestionsController.cs
@@ -14,34 +14,12 @@ namespace CNTTFAQ.Areas.Admin.Controllers
 {
     public class ManageQuestionsController : Controller
     {
-        QUANLYCAUHOIEntities model = new QUANLYCAUHOIEntities();
 
         // GET: Admin/AdminManageQuestions
         public ActionResult Index()
         {
 /*            var question = model.QUESTIONS.ToList().OrderByDescending(x => x.ID).ToList();
 */            return View();
-        }
-
-        [HttpGet]
-        public ActionResult Edit(int id)
-        {
-            var product = model.QUESTIONS.FirstOrDefault(x => x.ID == id);
-            ViewBag.product_type = model.CATEGORies.OrderByDescending(x => x.ID).ToList();
-            return View(product);
-        }
-
-        [HttpPost, ValidateInput(false)]
-        public ActionResult Edit(int id, QUESTION f, HttpPostedFileBase IMAGE_URL)
-        {
-            var product = model.QUESTIONS.FirstOrDefault(x => x.ID == id);
-            product.QUESTION_CODE = f.QUESTION_CODE;
-            product.QUESTION_NAME = f.QUESTION_NAME;
-            product.CATEGORY = f.CATEGORY;
-            product.CONTENT = f.CONTENT;
-            product.STATUS = f.STATUS;
-            model.SaveChanges();
-            return RedirectToAction("Index");
         }
     }
 }
