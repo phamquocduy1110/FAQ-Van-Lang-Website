d8d5b09b251c65050cb84d1a64ec27d8213532fb
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-07-01 22:13:53 +0700

    fix bug

--- CNTTFAQ/CNTTFAQ/Areas/Admin/Controllers/ManageCatalogController.cs
+++ CNTTFAQ/CNTTFAQ/Areas/Admin/Controllers/ManageCatalogController.cs
@@ -9,13 +9,10 @@ namespace CNTTFAQ.Areas.Admin.Controllers
 {
     public class ManageCatalogController : Controller
     {
-        DIEUBANTHUONGHOIWEBSITEEntities model = new DIEUBANTHUONGHOIWEBSITEEntities();
-
         // GET: Admin/AdminManageCatalog
         public ActionResult Index()
         {
-            var catalog = model.DANH_MUC.ToList().OrderByDescending(x => x.ID).ToList();
-            return View(catalog);
+            return View();
         }
     }
 }
