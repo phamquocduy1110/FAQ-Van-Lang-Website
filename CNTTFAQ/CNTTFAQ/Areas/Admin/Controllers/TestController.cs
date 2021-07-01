8175b7d2ec633a04b51d6046d87f56b7e5be1f80
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-07-01 21:58:51 +0700

    Create TestController

---
+++ CNTTFAQ/CNTTFAQ/Areas/Admin/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CNTTFAQ.Areas.Admin.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Admin/Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
