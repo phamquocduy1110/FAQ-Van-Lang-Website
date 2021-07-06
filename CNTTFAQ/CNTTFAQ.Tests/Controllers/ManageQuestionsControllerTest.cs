2cfe068ea2b2b93e4ec894f767084b37af3870fa
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-07-06 15:22:38 +0700

    Unit Test for load all list CAU_HOI

---
+++ CNTTFAQ/CNTTFAQ.Tests/Controllers/ManageQuestionsControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CNTTFAQ.Areas.Admin.Controllers;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using CNTTFAQ.Models;
+using System.Linq;
+using System;
+
+namespace CNTTFAQ.Tests.Controllers
+{
+    [TestClass]
+    public class ManageQuestionsControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            var controller = new ManageQuestionsController();
+
+            var result = controller.Index() as ViewResult;
+            Assert.IsNotNull(result);
+
+            var model = result.Model as List<CAU_HOI>;
+            Assert.IsNotNull(model);
+
+            var db = new DIEUBANTHUONGHOIWEBSITEEntities();
+            Assert.AreEqual(db.CAU_HOI.Count(), model.Count());
+        }
+
+        [TestMethod]
+        public void CreateG()
+        {
+            var controller = new ManageCatalogController();
+
+            var result = controller.Create() as ViewResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
