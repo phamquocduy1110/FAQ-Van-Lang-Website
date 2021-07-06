b0104917c186bce93b3b3ef10068e33beba7046d
187PM20475 - Vo Le Huy Hoang - K24T-PM1 <hoang.187pm20475@vanlanguni.vn>
2021-07-06 10:09:06 +0700

    del

--- CNTTFAQ/CNTTFAQ/Controllers/StatisticalController.cs
+++ CNTTFAQ/CNTTFAQ/Controllers/StatisticalController.cs
@@ -13,26 +13,6 @@ namespace CNTTFAQ.Controllers
         {
             return View();
         }
-        // GET: Home
-        public ActionResult Index()
-        {
-            List<DataPoint> dataPoints = new List<DataPoint>();
 
-            dataPoints.Add(new DataPoint("NXP", 14));
-            dataPoints.Add(new DataPoint("Infineon", 10));
-            dataPoints.Add(new DataPoint("Renesas", 9));
-            dataPoints.Add(new DataPoint("STMicroelectronics", 8));
-            dataPoints.Add(new DataPoint("Texas Instruments", 7));
-            dataPoints.Add(new DataPoint("Bosch", 5));
-            dataPoints.Add(new DataPoint("ON Semiconductor", 4));
-            dataPoints.Add(new DataPoint("Toshiba", 3));
-            dataPoints.Add(new DataPoint("Micron", 3));
-            dataPoints.Add(new DataPoint("Osram", 2));
-            dataPoints.Add(new DataPoint("Others", 35));
-
-            ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
-
-            return View();
-        }
     }
 }
