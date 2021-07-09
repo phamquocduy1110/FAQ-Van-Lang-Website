8abe3a50df369b3588a50ad7e59a2457351ae30f
Phạm Quốc Duy <55733660+phamquocduy1110@users.noreply.github.com>
2021-07-09 18:48:44 +0700

    Thiết kế sơ đồ thống kê

--- CNTTFAQ/CNTTFAQ/Controllers/AccountController.cs
+++ CNTTFAQ/CNTTFAQ/Controllers/AccountController.cs
@@ -88,7 +88,7 @@ namespace CNTTFAQ.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Invalid account. Please check your username or password");
                     return View(model);
             }
         }
