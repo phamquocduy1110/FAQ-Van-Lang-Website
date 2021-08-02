8a5dbc7e2a9541e9a24a8ee3571307d8837c06c3
phamquocduy1110 <phamquocduy1110@gmail.com>
2021-08-03 00:38:57 +0700

    Thay đổi giao diện trang login

--- CNTTFAQ/CNTTFAQ.Tests/Controllers/AdminHomeControllerTest.cs
+++ CNTTFAQ/CNTTFAQ.Tests/Controllers/AdminHomeControllerTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CNTTFAQ.Areas.Admin.Controllers;
+using CNTTFAQ.Models;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace CNTTFAQ.Tests.Controllers
@@ -10,26 +12,39 @@ namespace CNTTFAQ.Tests.Controllers
         [TestMethod]
         public void Index()
         {
-            // Arrange
-            AdminHomeController controller = new AdminHomeController();
+            DIEUBANTHUONGHOIWEBSITEEntities model = new DIEUBANTHUONGHOIWEBSITEEntities();
 
-            // Act
-            ViewResult result = controller.Index() as ViewResult;
+            var model1 = model.AspNetUsers.Count();
+            var model2 = model.CAU_HOI.Count();
+            var model3 = model.DANH_MUC.Count();
+            var model4 = model.CAU_HOI.Sum(x => x.LUOT_XEM);
+            var model5 = model.CAU_TRA_LOI.Count();
+            var model6 = model.GUI_CAU_HOI.Count();
+            var model7 = model.CAU_HOI.Where(x => x.DUYET_DANG != false).Count();
+            var model8 = model.CAU_HOI.Where(x => x.DUYET_DANG != true).Count();
+            var model9 = model.GUI_CAU_HOI.Where(x => x.TRANG_THAI != false).Count();
+            var model10 = model.GUI_CAU_HOI.Where(x => x.TRANG_THAI != true).Count();
 
-            // Assert
-            Assert.IsNotNull(result);
+            var db = new DIEUBANTHUONGHOIWEBSITEEntities();
+            Assert.AreEqual(db.AspNetUsers.Count(), model1);
+            Assert.AreEqual(db.CAU_HOI.Count(), model2);
+            Assert.AreEqual(db.DANH_MUC.Count(), model3);
+            Assert.AreEqual(db.CAU_HOI.Sum(x => x.LUOT_XEM), model4);
+            Assert.AreEqual(db.CAU_TRA_LOI.Count(), model5);
+            Assert.AreEqual(db.GUI_CAU_HOI.Count(), model6);
+            Assert.AreEqual(db.CAU_HOI.Where(x => x.DUYET_DANG != false).Count(), model7);
+            Assert.AreEqual(db.CAU_HOI.Where(x => x.DUYET_DANG != true).Count(), model8);
+            Assert.AreEqual(db.GUI_CAU_HOI.Where(x => x.TRANG_THAI != false).Count(), model9);
+            Assert.AreEqual(db.GUI_CAU_HOI.Where(x => x.TRANG_THAI != true).Count(), model10);
         }
 
         [TestMethod]
         public void Calendar()
         {
-            // Arrange
             AdminHomeController controller = new AdminHomeController();
 
-            // Act
             ViewResult result = controller.Calendar() as ViewResult;
 
-            // Assert
             Assert.IsNotNull(result);
         }
     }
