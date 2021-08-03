941eaa0a04d077d66b081af051a632414866a0b0
phamquocduy1110 <phamquocduy1110@gmail.com>
2021-08-04 05:59:46 +0700

    Thêm chức năng gửi mail

--- CNTTFAQ/CNTTFAQ/Areas/Admin/Controllers/StudentQuestionController.cs
+++ CNTTFAQ/CNTTFAQ/Areas/Admin/Controllers/StudentQuestionController.cs
@@ -21,25 +21,6 @@ namespace CNTTFAQ.Areas.Admin.Controllers
             return View(studentquestion);
         }
 
-        public ActionResult SendMail()
-        {
-            string recipient = Request["to"];
-            string subject = Request["subject"];
-            string body = Request["body"];
-
-            WebMail.SmtpServer = "smtp.gmail.com";
-            WebMail.SmtpPort = 587;
-            WebMail.SmtpUseDefaultCredentials = true;
-            WebMail.EnableSsl = true;
-
-            WebMail.UserName = "Email address";
-            WebMail.Password = "Password";
-
-            WebMail.Send(to: recipient, subject: subject, body: body, isBodyHtml: true);
-
-            return View();
-        }
-
         // GET: List of data from CAU_HOI /AdminManageQuestions
         public ActionResult Edit(int id)
         {
---
+++ CNTTFAQ/CNTTFAQ/Models/MailModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CNTTFAQ.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class MailModel
+    {
+        [Required(ErrorMessage = "Mời bạn nhập Email của Ban Chủ Nhiệm Khoa")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        [Display(Name = "Email address")]
+        public string From { get; set; }
+
+        [Required(ErrorMessage = "Mời bạn nhập Email người cần gửi")]
+        [EmailAddress(ErrorMessage = "Email không hợp lệ")]
+        [Display(Name = "Email address")]
+        public string To { get; set; }
+
+        [Required(ErrorMessage = "Mời bạn nhập tiêu đề Email")]
+        [MinLength(5, ErrorMessage = "Tiêu đề email phải tối thiếu ít nhất là 5 kí tự")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Mời bạn nhập nội dung")]
+        public string Body { get; set; }
+    }
+}
